8a9c17e6eec8de5e87e21610ee65ca5096b18cd2
Kuzin Dmitriy <SoraDnic@icloud.com>
2022-08-06 22:08:11 +0300

    Added MagicBox's object for integer

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
 public class Main {
     public static void main(String[] args) throws RuntimeException {
-        MagicBox stringBox = new MagicBox(5);
+        MagicBox<String> stringBox = new MagicBox<String>(5);
 
         stringBox.add("One");
         stringBox.add("Two");
         stringBox.add("three");
         stringBox.add("Four");
-        //stringBox.add("Five");
-
+        stringBox.add("Five");
         System.out.println(stringBox.pick());
+
+        MagicBox<Integer> intBox = new MagicBox<Integer>(5);
+        intBox.add(1);
+        intBox.add(2);
+        intBox.add(3);
+        intBox.add(4);
+        //intBox.add(5);
+        System.out.println(intBox.pick());
+
+
     }
 }
